90a8e19b22f4f98c4afd02e832a1dd3d023d654b
Jhosetp Francisco <jhosetp.chino@hotmail.com>
2020-11-17 12:30:13 -0500

    Adding Config folder and file

---
+++ config/index.ts
@@ -0,0 +1,9 @@
+// Almacena parámetros de configuración
+const config = {
+    "endpoint": "https://jf-database.documents.azure.com:443/",
+    "key": "jF5AbK4Bt05i6EYQkcUFqiFQGV72ClwNJBVXxBgTREsKfDQdL0zcEsqgfgEbvoRLFKZlBIPDWKJDojLdtGn2nA==",
+    "databaseId": "JF_Test",
+    "containerId": "Usuario"
+}
+
+export default config;
--- database/index.ts
+++ database/index.ts
@@ -1,11 +1,12 @@
 import { Container, CosmosClient, Database } from '@azure/cosmos';
+import config from '../config';
 // Declaración directa de los parametros para la conexión al Cosmos DB
 // Recomendable realizar llamadas de variables de entorno
 // Estos parámetros son para testing, la llave puede estar en deshuso
-const endpoint:string = "https://jf-database.documents.azure.com:443/";
-const key:string = "jF5AbK4Bt05i6EYQkcUFqiFQGV72ClwNJBVXxBgTREsKfDQdL0zcEsqgfgEbvoRLFKZlBIPDWKJDojLdtGn2nA==";
-const databaseId:string = "JF_Test";
-const containerId:string = "Usuario";
+const endpoint:string = config.endpoint;
+const key:string = config.key;
+const databaseId:string = config.databaseId;
+const containerId:string = config.containerId;
 // Declarando Objetos necesarios
 const client:CosmosClient = new CosmosClient({ endpoint, key });
 const database:Database = client.database(databaseId);
